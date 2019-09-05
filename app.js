edcac418272c7a915a65bb1cb67d4058d550ba24
MatthCanardo <43275151+MatthCanardo@users.noreply.github.com>
2019-09-05 09:16:51 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -25,100 +25,103 @@ pvPlayer[1].textContent = `${player1.pv}/${pvMax} pv(s)`;
 names[0].textContent = player2.nom;
 names[1].textContent = player1.nom;
 
-// Évènement(s)
+// Évènement(s) clique.
 start.addEventListener('click', function(){
    
+    // Interval 
     const actionPlayer1 = setInterval(function(){
+        // Si le combat n'est pas continuable car pv = 0.
         if (player1.pv <= 0) {
             clearInterval(actionPlayer1);
             texte.textContent = `${player2.nom} a gagné`;
         }
-        // A modifié, à mettre dans une autre condition.
-        
         else{
             // Selection d'une attaque aléatoire pour player 1. 
             const mouvementPlayer1 = Math.floor(Math.random() * 2);
-            // Condition selon l'attaque.
+            // Si le random = charge.
             if (mouvementPlayer1 == 0){
+                // Si le combat n'est pas continuable car pv = 0.
                 if (player2.pv <= 0){
-                    
                     clearInterval(actionPlayer1);
-                    
                 }
+                // Sinon utilisation normal de charge.
                 else{
                     player2.pv = player2.pv - player1.charge
                     texte.textContent = `${player1.nom} utilise charge`;
                     player2.pv <= 0 ? player2.pv = 0 : "";
-                    // Pv(s) textContent
+                    // Pv(s) textContent.
                     pvPlayer[1].textContent = `${player1.pv}/${pvMax} pv(s)`;
                     pvPlayer[0].textContent = `${player2.pv}/${pvMax} pv(s)`;
-                    console.log(texte.textContent);
                 }
             }
+            // Si le random = soin
             if (mouvementPlayer1 == 1){
+                // Si le combat n'est pas continuable car pv = 0.
                 if (player2.pv <= 0){
                     clearInterval(actionPlayer1);  
                 }
+                // Sinon utilisation normal de soin.
                 else{
                     player1.pv = player1.pv + player1.soin
                     texte.textContent = `${player1.nom} se soigne`;
-
                     // Pv(s) textContent
                     pvPlayer[1].textContent = `${player1.pv}/${pvMax} pv(s)`;
                     pvPlayer[0].textContent = `${player2.pv}/${pvMax} pv(s)`;
-                    console.log(texte.textContent);
                 }
             }
         }
-        
-        console.log(`${player1.nom} a joué `, player1.nom, player1.pv , player2.nom, player2.pv);
+        // Test dans la console :
+        //console.log(`${player1.nom} a joué `, player1.nom, player1.pv , player2.nom, player2.pv);
         
     }, 4000)
 
+    // Déclanchement de l'action 
     setTimeout(function(){
+        // Interval
         const actionPlayer2 = setInterval(function(){
+            // Si le combat n'est pas continuable car pv = 0.
             if (player2.pv <= 0) {
                 clearInterval(actionPlayer2);
-                
                 texte.textContent = `${player1.nom} a gagné`;
             }
             else{
-                
                 // Selection d'une attaque aléatoire pour player 2. 
                 const mouvementPlayer2 = Math.floor(Math.random() * 2);
-                mouvementPlayer2 == 0 ? player1.pv = player1.pv - player2.charge : player2.pv = player2.pv + player2.soin;
+                // Si random = charge
                 if (mouvementPlayer2 == 0){
+                    // Si le combat n'est pas continuable car pv = 0.
                     if (player1.pv <= 0){
-                        
                         clearInterval(actionPlayer2); 
                     }
+                    // Sinon utilisation normal de charge
                     else{
+                        player1.pv = player1.pv - player2.charge;
                         texte.textContent = `${player2.nom} utilise charge`;
                         player1.pv <= 0 ? player1.pv = 0 : "";
                         pvPlayer[0].textContent = `${player2.pv}/${pvMax} pv(s)`;
                         pvPlayer[1].textContent = `${player1.pv}/${pvMax} pv(s)`;
-                        }
-                    
+                    }
                 }
+                // Si random = soin
                 if (mouvementPlayer2 == 1 ){
+                    // Si le combat n'est pas continuable car pv = 0.
                     if (player1.pv <= 0){
                         clearInterval(actionPlayer2);  
                     }
+                    // Sinon utilisation normal de soin
                     else{
+                        player2.pv = player2.pv + player2.soin;
                         texte.textContent = `${player2.nom} se soigne`;
                         pvPlayer[0].textContent = `${player2.pv}/${pvMax} pv(s)`;
                         pvPlayer[1].textContent = `${player1.pv}/${pvMax} pv(s)`;
-                    }
-                    
+                    }  
                 }
             }
-            console.log(`${player2.nom} a joué`,player1.nom, player1.pv, player2.nom, player2.pv);
-            }, 4000)
-        
+            // Test dans la console : 
+            //console.log(`${player2.nom} a joué`,player1.nom, player1.pv, player2.nom, player2.pv);
+            }, 4000) 
     }, 2000)
-    
-    
 });
 
-// Console.log()
-console.log(player1, player2);
+// Test dans la console :
+//console.log(player1, player2);
